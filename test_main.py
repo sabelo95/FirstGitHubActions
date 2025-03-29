117b60cf2ce04f65db2393ead8f8e16a02016da7
santiago berrio <santiagoberriolopez@gmail.com>
2025-03-29 07:55:45 -0500

    Ready for firt push

---
+++ test_main.py
@@ -0,0 +1,9 @@
+import unittest
+from main import add
+
+class TestAddFunction(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2,3),5)
+        self.assertEqual(add(-1,1),0)
+        self.assertEqual(add(0,0),0)
+        
